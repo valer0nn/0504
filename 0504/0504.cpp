a9b2e8ab0535ff33784220a039db3ac39d3ba7c5
Valeriya Kosykh <valeriya.kosyh2001@gmail.com>
2024-04-10 14:34:14 +0300

    commit3

--- 0504/0504.cpp
+++ 0504/0504.cpp
@@ -1,1 +1,12 @@
-﻿
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+
+    V1 = 1.27 * (Q / d * d);
+    hl = (D * L / d) * ((V1 * V1) / 19.6);
+    hm = 0.1 * hl; 
+    cout << "N=" << N << endl;
+}
+
